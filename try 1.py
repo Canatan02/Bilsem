aaa9aaf5eba7e791b557468e79a6f5346603f71c
Canatan02 <92210203+Canatan02@users.noreply.github.com>
2022-01-08 13:34:09 +0300

    Add files via upload

---
+++ try 1.py
@@ -0,0 +1,4 @@
+#print(1/0) #zero division hatası üretir
+#print(a) #böyle bir değişken olmadığı için name error hatası üretir
+#int("5a") #value error hatası üretir çünkü 5a bir sayı değildir
+#print("Bilsem"deneme) #syntax error hatası üretir
---
+++ try uygulama.py
@@ -0,0 +1,8 @@
+try:
+    a=int(input("1. Sayıyı Giriniz:"))
+    b=int(input("2. Sayıyı Giriniz:"))
+    print(a/b)
+except ValueError:
+    print("Sayı gir dedik!")
+
+
